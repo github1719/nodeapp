7c307427db39a1424fc796300129e962b5c3ef35
github1719 <39268329+github1719@users.noreply.github.com>
2022-08-30 21:37:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node baba enjoy!\n'
+  const msg = 'Hello Node baba enjoy one!\n'
   res.end(msg);
 });
 
